a997e30cb1f3bbef597a576976657802ad6e2d3a
Gurpreet Singh <gursingh7@publicisgroupe.net>
2024-05-21 14:17:45 +0200

    build: vscode settings

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,9 +3,6 @@
 // docs: Documentation only changes
 // feat: A new feature
 // fix: A bug fix
-// perf: A code change that improves performance
-// refactor: A code change that neither fixes a bug nor adds a feature
-// style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
 
 module.exports = {
@@ -29,22 +26,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      [
-        "build",
-        "chore",
-        "ci",
-        "docs",
-        "feat",
-        "fix",
-        "perf",
-        "refactor",
-        "revert",
-        "style",
-        "test",
-        "translation",
-        "security",
-        "changeset",
-      ],
+      ["build", "ci", "docs", "feat", "fix", "revert", "test", "security"],
     ],
   },
 };
